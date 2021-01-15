9e6ab7bc9691d86d6e09d555c7f2b482bfb1bf81
minjae-mj <65207835+minjae-mj@users.noreply.github.com>
2021-01-15 15:45:30 +0900

    Modify nav structure according to atomic pattern

---
+++ src/App/atoms/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+interface ButtonProps {
+  text: string; 
+}
+
+const Button = ({ text }: ButtonProps) => {
+  return (
+    <button>{text}</button>
+  )
+}
+
+export default Button; 
--- src/App/organisms/NavBar.tsx
+++ src/App/organisms/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import Logo from '../atoms/Logo'; 
-import NavBtn from '../atoms/NavBtn'; 
+import NavBtn from '../molecules/NavBtn'; 
 
 interface NavBtnProps {
   isLogin: boolean;
