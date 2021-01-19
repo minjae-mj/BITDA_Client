37189248cc9fdd37c8f8118a4b35f240dbe6d741
dawa93 <dawa93@naver.com>
2021-01-19 14:49:19 +0900

    Add functions to Main page

--- src/App/atoms/MainSelectBtn.tsx
+++ src/App/atoms/MainSelectBtn.tsx
@@ -1,25 +1,60 @@
-import React from 'react'; 
+import React,{useState} from 'react'; 
 import styled from 'styled-components'; 
 
-type Props = {
-  text: string;    
+interface BtnProps {
+  type: string;
+  buttonList : string[];
 }
+const StyledButton = styled.button`
+  color: black;
+  // background-color: #F6F5F5; 
+  border: 1px solid #EE6F57; 
+  border-radius: 4px; 
+  font-size: 12px; 
+  padding: 4px 8px;
+  outline : none;
+  cursor : pointer;
+  &:hover{
+    color: #F6F5F5; 
+    background-color: #EE6F57;
+  }
+` ;
+const ActiveBtn = styled(StyledButton)`
+    color: #F6F5F5; 
+    background-color: #EE6F57;
+`;
+const MainSelectBtn = ({ type,buttonList}: BtnProps ): JSX.Element => {
+  const [isClicked, setIsClicked] = useState('');
+  
+  let clickHandler = (e : any) : void => {
+    let target = e.target.innerText;  // 1만원 이하... 등등
+    let type = e.target.dataset.type; // type, price, taste, alcohol
+    
+    if(isClicked === target){
+      setIsClicked('')
+    }else{
+      setIsClicked(target)
+    }
 
-// const StyleLandingBtn = styled.button`
-//   color: #F6F5F5; 
-//   background-color: #ED6663; 
-//   border: 1px solid #ED6663; 
-//   border-radius: 5px; 
-//   font-size: 12px; 
-//   width: 100px; 
-//   height: 40px; 
-// ` 
-
-const LandingBtn = ({ text }: Props ): JSX.Element=> {
-
+    console.log(type)
+    console.log(isClicked)
+  }
+  
   return (
-    <button>{text}</button>
+    // <div>
+    // { !isClicked? 
+    //       <StyledButton data-type={type} onClick={clickHandler}>{text}</StyledButton>
+    //       :<ActiveBtn data-type={type} onClick={clickHandler}>{text}</ActiveBtn>
+    // }
+    // </div>
+    <div>
+      {buttonList.map((button)=>(
+        button === isClicked? 
+        <ActiveBtn data-type={type} onClick={clickHandler}>{button}</ActiveBtn> 
+        :
+        <StyledButton data-type={type} onClick={clickHandler}>{button}</StyledButton>
+      ))}
+    </div>
   )
 }
-
-export default LandingBtn; 
+export default MainSelectBtn; 
