5d56cfb49aafa43d2100a5b689cae69118549a71
qkrwogns9703 <jaehunb350@gmail.com>
2021-01-26 14:09:08 +0900

    Modify navbar useEffect

--- src/App/organisms/NavBar.tsx
+++ src/App/organisms/NavBar.tsx
@@ -1,24 +1,39 @@
-import React, { useEffect } from 'react'; 
-import Logo from '../atoms/Logo'; 
-import NavBtn from '../molecules/NavBtn'; 
-
+import React, { useEffect } from 'react';
+import Logo from '../atoms/Logo';
+import NavBtn from '../molecules/NavBtn';
+import server, { clientURL } from '../../apis/server';
 
 const NavBar = (): JSX.Element => {
   // let isLogin = localStorage.getItem('isLogin')
+  const accessToken = localStorage.getItem('accessToken');
+  useEffect(() => {
+    if (accessToken) {
+      getUserInfo();
+    }
+  }, []);
 
-  // useEffect(()=>{
-  //   // window.location.reload();
-  // },[])
-
-
-  
+  let getUserInfo = async () => {
+    try {
+      console.log('정보 요청d');
+      let user = await server.get('/users/mypage', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const { data } = user;
+      console.log(data);
+      //setUserInfo({ userInfo, ...data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <nav>
       <Logo />
       <NavBtn />
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar; 
+export default NavBar;
