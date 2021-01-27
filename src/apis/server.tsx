d7a4f72d0759c9c7e8021e5f2a725b773c5df98b
dawa93 <dawa93@naver.com>
2021-01-27 10:45:08 +0900

    add button function and footer to landing page

--- src/apis/server.tsx
+++ src/apis/server.tsx
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const clientURL = 'http://localhost:3000'
 
 export default axios.create({
-    baseURL: 'https://www.bitda.ml'
-    // baseURL: 'http://localhost:8080'
+    // baseURL: 'https://www.bitda.ml'
+    baseURL: 'http://localhost:8080'
 });
