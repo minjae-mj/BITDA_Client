c2905d0c30eae972c8b872a2512f762fa42ece22
dawa93 <dawa93@naver.com>
2021-01-28 22:37:20 +0900

    add a stamp to footer

--- src/App/atoms/FooterStamp.tsx
+++ src/App/atoms/FooterStamp.tsx
@@ -1,8 +1,21 @@
 import React from 'react'; 
+import styled from 'styled-components';
+import bitdaStamp from '../../images/bitdaStamp.png'
+
+const StyledDiv = styled.div`
+  height : 50%;
+  padding-right : 2.4rem;
+`;
+
+const StyledImg = styled.img`
+  height : 100%;
+`;
 
 const FooterStamp = (): JSX.Element => {
   return (
-    <div>Footer Stamp</div>
+    <StyledDiv>
+      <StyledImg src={bitdaStamp} alt='bitda stamp'></StyledImg>
+    </StyledDiv>
   )
 }
 
