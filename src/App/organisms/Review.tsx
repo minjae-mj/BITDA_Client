c0dadc0722ac7ebe7165dcbbceedfe027465c572
qkrwogns9703 <jaehunb350@gmail.com>
2021-01-30 22:07:37 +0900

    Modify drinkDetail responsive css

--- src/App/organisms/Review.tsx
+++ src/App/organisms/Review.tsx
@@ -1,115 +1,138 @@
-import React, { useState, useEffect } from 'react'; 
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import ReviewCard from '../molecules/ReviewCard';
-import ReviewInput from '../molecules/ReviewInput'; 
-import styled from 'styled-components'; 
-import server from '../../apis/server'; 
+import ReviewInput from '../molecules/ReviewInput';
+import styled from 'styled-components';
+import server from '../../apis/server';
 import { RootState } from '../../reducers';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateReviews } from '../../actions'; 
+import { updateReviews } from '../../actions';
 import LeftArrow from '../atoms/LeftArrow';
 import RightArrow from '../atoms/RightArrow';
 
 interface Params {
-  drinkId: string; 
+  drinkId: string;
 }
 
 const Review = () => {
-  const { drinkId }: Params = useParams(); 
-  const [idx , setIdx] = useState(0);
-
-  let state = useSelector((state :RootState ) => state.reviewListReducer.reviewList);
+  const { drinkId }: Params = useParams();
+  const [idx, setIdx] = useState(0);
+  const [reviewLength, setReviewLength] = useState(4);
+  let state = useSelector(
+    (state: RootState) => state.reviewListReducer.reviewList
+  );
   const dispatch = useDispatch();
 
-
   const getReviewList = async () => {
-    const reviewList = await server.get(`/reviews/list/${drinkId}`); 
-    const { data }: any = reviewList; 
+    const reviewList = await server.get(`/reviews/list/${drinkId}`);
+    const { data }: any = reviewList;
     dispatch(updateReviews(data.reviews));
-  }
+  };
 
   useEffect(() => {
-    getReviewList(); 
-  }, []); 
+    getReviewList();
+  }, [reviewLength]);
 
-  let fourReviews = state.slice(idx, idx+4);
+  window.addEventListener('resize', () => {
+    let width = document.body.clientWidth;
+    //setWindowX(width);
+    if (width < 1440) {
+      setReviewLength(3);
+    } else if (width > 1440) {
+      setReviewLength(4);
+    }
+  });
+  let fourReviews = state.slice(idx, idx + reviewLength);
   let nextReviewsHandler = () => {
-    if(idx + 4  < state.length ){
-      setIdx(pre => pre + 4)
-    }else{
+    if (idx + reviewLength < state.length) {
+      setIdx((pre) => pre + reviewLength);
+    } else {
       return;
     }
-  }
+  };
   let preReviewsHandler = () => {
-    if(idx - 4  >= 0 ){
-      setIdx(pre => pre - 4)
-    }else{
+    if (idx - reviewLength >= 0) {
+      setIdx((pre) => pre - reviewLength);
+    } else {
       return;
     }
-  }
+  };
 
-  const [leftHover, setLeftHover] = useState(false); 
-  const [rightHover, setRightHover] = useState(false); 
-
+  const [leftHover, setLeftHover] = useState(false);
+  const [rightHover, setRightHover] = useState(false);
   return (
     <StyleReview>
       <StyleText>리뷰</StyleText>
       <StyleReviewList>
-        <div onMouseEnter={() => setLeftHover(true)} onMouseLeave={() => setLeftHover(false)}>
-          <LeftArrow handleClick={preReviewsHandler} fill={leftHover? "#ED6663": "#C4C4C4"} />
+        <div
+          onMouseEnter={() => setLeftHover(true)}
+          onMouseLeave={() => setLeftHover(false)}
+        >
+          <LeftArrow
+            handleClick={preReviewsHandler}
+            fill={leftHover ? '#ED6663' : '#C4C4C4'}
+          />
         </div>
         <StyleReviewCard>
-          {!state.length ? <StyleNoReivew>첫 리뷰를 작성해주세요.</StyleNoReivew> : 
-            fourReviews.map(review => {
-              return <ReviewCard key={review.id} review={review} drinkId={drinkId} />
+          {!state.length ? (
+            <StyleNoReivew>첫 리뷰를 작성해주세요.</StyleNoReivew>
+          ) : (
+            fourReviews.map((review) => {
+              return (
+                <ReviewCard key={review.id} review={review} drinkId={drinkId} />
+              );
             })
-          }
+          )}
         </StyleReviewCard>
-        <div onMouseEnter={() => setRightHover(true)} onMouseLeave={() => setRightHover(false)}>
-          <RightArrow handleClick={nextReviewsHandler} fill={rightHover? "#ED6663": "#C4C4C4"} />
+        <div
+          onMouseEnter={() => setRightHover(true)}
+          onMouseLeave={() => setRightHover(false)}
+        >
+          <RightArrow
+            handleClick={nextReviewsHandler}
+            fill={rightHover ? '#ED6663' : '#C4C4C4'}
+          />
         </div>
-      </StyleReviewList> 
+      </StyleReviewList>
       <div>
-         <ReviewInput drinkId={drinkId} />
-      </div>  
-      
+        <ReviewInput drinkId={drinkId} />
+      </div>
     </StyleReview>
-  )
+  );
+};
 
-}
-
-export default Review; 
+export default Review;
 
 const StyleReview = styled.div`
-  display: flex; 
-  flex-direction: column; 
-  width: 80%; 
-  margin: 10rem auto; 
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  margin: 10rem auto;
 
-  // border: 1px solid green; 
-`
+  // border: 1px solid green;
+`;
 const StyleText = styled.p`
-  font-size: 2rem; 
-  color: var(--color-primary); 
-`
+  font-size: 2rem;
+  color: var(--color-primary);
+`;
 const StyleReviewList = styled.div`
-  display: flex; 
-  justify-content: space-around; 
-  align-items: center; 
-  height: 28rem; 
-  margin-top: 2rem; 
-`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 28rem;
+  margin-top: 2rem;
+`;
 const StyleReviewCard = styled.div`
-  display: flex; 
-  width: 100%; 
-  height: inherit; 
-  align-items: center; 
-`
+  display: flex;
+  width: 100%;
+  height: inherit;
+  align-items: center;
+`;
 const StyleNoReivew = styled.div`
-  width: 100%; 
-  font-size: 1.8rem; 
-  color: var(--color-primary); 
+  width: 100%;
+  font-size: 1.8rem;
+  color: var(--color-primary);
   text-align: center;
 
-  // border: 1px solid black; 
-`
+  // border: 1px solid black;
+`;
--- src/App/templates/DrinkDetailTemplate.tsx
+++ src/App/templates/DrinkDetailTemplate.tsx
@@ -1,22 +1,21 @@
-import React from 'react'; 
-import DrinkDesc from '../organisms/DrinkDesc'; 
-import Review from '../organisms/Review'; 
-import styled from 'styled-components'; 
+import React from 'react';
+import DrinkDesc from '../organisms/DrinkDesc';
+import Review from '../organisms/Review';
+import styled from 'styled-components';
 
 const StyleDrinkDetailTemplate = styled.div`
-  max-width: 100%;
-  margin: 1rem auto; 
-  margin-top: 12.2rem; 
-`
+  max-width: 80%;
+  margin: 1rem auto;
+  margin-top: 12.2rem;
+`;
 
 const DrinkDetailTemplate = (): JSX.Element => {
-  
   return (
     <StyleDrinkDetailTemplate>
       <DrinkDesc />
       <Review />
     </StyleDrinkDetailTemplate>
-  )
-}
+  );
+};
 
-export default DrinkDetailTemplate; 
+export default DrinkDetailTemplate;
